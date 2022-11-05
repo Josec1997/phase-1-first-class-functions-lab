ee8d2698d85a8a815d4b321ca5a0e78df79364c3
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-04 23:13:34 -0500

    commit tree d62bbfa1d997210790c0ac1dcf08bae27ed9084f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers( function() {
+//     console.log (['Antonio','Nuru'])
+// })
+
+function returnFirstTwoDrivers() {
+ return (['Antonia','Nuru'])
+}
+
+function returnLastTwoDrivers() {
+    return (['Amari', 'Mo'])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare){
+    return function (fare1){
+        return fare * fare1
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(parameter1,parameter2){
+    return parameter2(parameter1)
+}
